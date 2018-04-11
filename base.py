1084aa4f7ef1deaa639fe4f721215923caa79487
guojiming <1697200598@qq.com>
2018-04-11 21:25:49 +0800

    add lgm

--- base.py
+++ base.py
@@ -42,9 +42,32 @@ def result(model):
     result = pd.DataFrame({'instance_id':test['instance_id'], 'predicted_score':y_pred})
     result.to_csv('result/result0411.txt',sep=" ",index=False)
 
+def lgb_log_loss():
+    lgb0 = lgb.LGBMClassifier(
+        objective='binary',
+        # metric='binary_error',
+        num_leaves=35,
+        depth=8,
+        learning_rate=0.05,
+        seed=2018,
+        colsample_bytree=0.8,
+        # min_child_samples=8,
+        subsample=0.9,
+        n_estimators=20000)
+    X = train[select_cols]
+    Y = train['is_trade']
+    X_train,X_test,y_train,y_test = train_test_split(X,Y,test_size=0.4, random_state=0)
+    print("Training...")
+    lgb_model = lgb0.fit(X_train, y_train, eval_set=[(X_test, y_test)], early_stopping_rounds=200)
+    print("Predicting...")
+    y_prediction = lgb_model.predict_proba(X_test)
+    test_pred = y_prediction[:, 1]
+    print('log_loss ', log_loss(y_test, test_pred))
+
 if __name__ == "__main__":
+    lgb_log_loss()
     #print(test['instance_id'].values)
-    result(LogisticRegression(C=100,n_jobs=-1))
+    #result(LogisticRegression(C=100,n_jobs=-1))
     #print("---LogisticRegression------")
     #model_log_loss(LogisticRegression(C=100,n_jobs=-1))
     # print("---DecisionTreeClassifier---")
@@ -53,3 +76,5 @@ if __name__ == "__main__":
     # model_log_loss(RandomForestClassifier())
     #print("---SVC---")
     #model_log_loss(SVC())
+
+    
